d88bceab87705f5e4d888a56821c0c4d12fa570c
2401734 <natan.araujo@aluno.faculdadeimpacta.com.br>
2025-03-14 20:30:56 -0300

    first commit

---
+++ src/Exercicio2.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        double fahrenheit = Double.parseDouble(JOptionPane.showInputDialog(null,"Digite a temperatura em Fahrenheit: "));
+        double conversao = (fahrenheit - 32) / 1.8;
+        JOptionPane.showMessageDialog(null,"Valor em Celsius: " + String.format("%.2f",conversao));
+
+
+    }
+}
